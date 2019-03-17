154fcb37bd08a70d4480aca4812ba9030899613a
fibersel <0583463@gmail.com>
2019-03-17 20:26:23 +0300

    indents fixed, exception cathing replaced with reference

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -1,7 +1,6 @@
 #include "numbers.dat"
 #include <cstdlib>
 #include <iostream>
-#include <stdexcept>
 
 
 
@@ -12,43 +11,44 @@ void sieve_fill(bool * prime, size_t size){
 	for(size_t i = 2; i < size; i++)
 		prime[i] = true;
 	prime[1] = prime[0] = false;
-	for(unsigned long long i = 2; i < size; i++)	
+	for(unsigned long long i = 2; i < size; i++)
 		if(prime[i])
-			if(i * i < size)
+			if(i * i <= size)
 				for(unsigned long long j = i * i; j < size; j += i)
 					prime[j] = false;	
-	}
+}
 
 
 size_t count_prime(bool * prime, size_t size, size_t leftIndex, size_t rightIndex){
 	size_t ctr = 0;
 	if(leftIndex > size || rightIndex > size)
 		throw std::out_of_range("Any index is out of range!");
-	for(size_t i = leftIndex; i <= rightIndex;i++)
-			if(prime[Data[i]])
-				ctr++;	
+	for(size_t i = leftIndex; i <= rightIndex; i++)
+		if(prime[Data[i]])
+			ctr++;
+
 	return ctr;
-	}
+}
 
 
-int main(int argc, char* argv[]){	
+int main(int argc, char* argv[]){
 	if(argc == 1 || argc % 2 == 0)
 		return -1;
 	
 	bool * prime = new bool[MAXVAL];
 	sieve_fill(prime, MAXVAL);
-	
+
 	int leftBound, rightBound;
 	for (int i = 1; i < argc; i += 2)
 	{
 		leftBound = std::atoi(argv[i]);
 		rightBound = std::atoi(argv[i + 1]);
-		
-		
+
+
 		size_t leftIndex = 0, rightIndex = Size - 1;
 		while(Data[leftIndex] < leftBound)
 			leftIndex++;
-			
+
 		while(Data[rightIndex] > rightBound)
 			rightIndex--;
 
@@ -57,21 +57,22 @@ int main(int argc, char* argv[]){
 			std::cout << 0 <<std::endl;
 			continue;
 		}
-		
+
 		size_t counter; 
 		try
-		{	
+		{
 			counter = count_prime(prime, MAXVAL, leftIndex, rightIndex);
 		} 
-		catch(std::out_of_range e)
+		catch(std::out_of_range& e)
 		{
-			std::cout <<"any bound is out of range! " << e.what();
+			std::cout <<"any bound is out of range!" << e.what();
 			return -1;
 		}
 		std::cout << counter << std::endl;
 	}
-	delete[] prime;
+	delete [] prime;
 
 	return 0;
 }
+
 

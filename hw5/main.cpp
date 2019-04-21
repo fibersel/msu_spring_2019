6a606622d9a3528a5c32b7ef1d8a4e9a4195c248
fibersel <0583463@gmail.com>
2019-04-22 01:33:57 +0300

    hw 5 made

---
+++ hw5/main.cpp
@@ -0,0 +1,44 @@
+#include <thread>
+#include <iostream>
+#include <atomic>
+
+#define MAX_PASS 500000
+
+void ping(std::atomic<int>& flag) {
+    int ctr = 0;
+    while (ctr < MAX_PASS) {
+        auto val = flag.load();
+        if(val  % 2 == 0) {
+            std::cout << "ping" << std::endl;
+            ctr++;
+            flag.store(val + 1);
+        };
+    };
+};
+
+
+void pong(std::atomic<int>& flag) {
+    int ctr = 0;
+    while (ctr < MAX_PASS) {
+        auto val = flag.load();
+        if(val  % 2 == 1) {
+            std::cout << "pong" << std::endl;
+            ctr++;
+            flag.store(val + 1);
+        };
+    };
+};
+
+
+int main() {
+
+    std::atomic<int> flag(0);
+
+    std::thread thread_1(ping, std::ref(flag));
+    std::thread thread_2(pong, std::ref(flag));
+
+
+    thread_1.join();
+    thread_2.join();
+    return 0;
+};

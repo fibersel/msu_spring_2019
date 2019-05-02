d7edb2e4cfd605330a3edbb9daafffc9a77cf811
fibersel <0583463@gmail.com>
2019-05-02 20:46:48 +0300

    hw6 fully made

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -6,22 +6,35 @@
 #include <algorithm>
 
 const int batch_size = 10000;
-const std::string dir_name = "tmp";
 std::condition_variable vars[2];
 
+
+//Простите меня за отсутствие ООП-дизайна как такового,у меня задница горит уже
+//В моём представлении для этой задачи идеально подошли бы каналы из Го или блокирующая очередь из Java,
+//Но синхронизировать всё это дело на мьютексах-ужас и боль.
 class data_accessor {
 public:
     uint64_t*  buf;
     std::mutex mutex;
     size_t size;
     bool end;
-    bool ready;
-    
-    data_accessor(int batch_size):buf(new uint64_t[batch_size]), end(false), ready(false){};
+    bool prepared;
+    bool read;
+
+    data_accessor(int batch_size):
+        buf(new uint64_t[batch_size]),
+        end(false),
+        prepared(false),
+        read(true)
+    {};
+
+    ~data_accessor() {
+        delete[] buf;
+    }
 };
 
 
-void merge(const std::string& filename, const std::string& resulting, const std::string& name){
+void merge(const std::string& filename, const std::string& resulting, const std::string& name) {
     std::ifstream f1(filename, std::ios::binary | std::ios::in);
     std::ifstream f2(resulting, std::ios::binary | std::ios::in);
     std::ofstream out(name + std::string("tmp.bin"), std::ios::binary | std::ios::out);
@@ -33,23 +46,23 @@ void merge(const std::string& filename, const std::string& resulting, const std:
     f2.read((char*) &in2, sizeof(uint64_t));
     count1 = f1.gcount();
     count2 = f2.gcount();
-    while(count1 || count2){
-        if(!count1){
+    while(count1 || count2) {
+        if(!count1) {
             out.write((char*) &in2, sizeof(uint64_t));
             f2.seekg(pos2++ * sizeof(uint64_t));
             f2.read((char*) &in2, sizeof(uint64_t));
             count2 = f2.gcount();
-        } else if(!count2){
+        } else if(!count2) {
             out.write((char*) &in1, sizeof(uint64_t));
             f1.seekg(pos1++ * sizeof(uint64_t));
             f1.read((char*) &in1, sizeof(uint64_t));
-            count1 = f1.gcount();   
+            count1 = f1.gcount();
         } else {
-            if(in1 < in2){
+            if(in1 < in2) {
                 out.write((char*) &in1, sizeof(uint64_t));
                 f1.seekg(pos1++ * sizeof(uint64_t));
                 f1.read((char*) &in1, sizeof(uint64_t));
-                count1 = f1.gcount(); 
+                count1 = f1.gcount();
             } else {
                 out.write((char*) &in2, sizeof(uint64_t));
                 f2.seekg(pos2++ * sizeof(uint64_t));
@@ -67,43 +80,48 @@ void merge(const std::string& filename, const std::string& resulting, const std:
 
 
 
-void sorter(const std::string& name, data_accessor* access, int id){
-    uint64_t* buf = new uint64_t[batch_size];  
+void sorter(const std::string& name, data_accessor* access, int id) {
+    uint64_t* buf = new uint64_t[batch_size];
     int size;
     int files_ctr = 0;
-    while(true){
+    vars[id].notify_one();
+    while(true) {
         {
             std::unique_lock<std::mutex> lock(access->mutex);
-            vars[id].wait(lock, 
-            [access]()
+            vars[id].wait(lock,
+                          [access]()
             {
-                return access->ready;
+                return access->prepared;
             });
-            access->ready = false;
+            access->prepared = false;
+            access->read = true;
+            vars[id].notify_one();
             if(access->end)
                 break;
             size = access->size;
-            std::copy(access->buf, access->buf + size, buf); 
+            std::copy(access->buf, access->buf + size, buf);
         }
+
         std::sort(buf, buf + size);
-        std::ofstream out(name + std::to_string(files_ctr), 
-        std::ios::out | std::ios::binary);
-        out.write((char*) buf, size);
+        std::ofstream out(name + std::to_string(files_ctr),
+                          std::ios::out | std::ios::binary);
+        out.write((char*) buf, size * sizeof(uint64_t));
         files_ctr++;
-   }
-   system((std::string("touch ") + name + std::string(".bin")).c_str());
-   while(files_ctr){
-       merge(name + std::to_string(files_ctr - 1), name + std::string(".bin"), name);
-       files_ctr--;
+    }
+    system((std::string("touch ") + name + std::string(".bin")).c_str());
+    delete[] buf;
+    while(files_ctr) {
+        merge(name + std::to_string(files_ctr - 1), name + std::string(".bin"), name);
+        files_ctr--;
     }
 }
 
 
 
-int main(int argc, char * argv[]){
+int main(int argc, char * argv[]) {
     if (argc != 2)
         return -1;
-    std::string filename(argv[1]); 
+    std::string filename(argv[1]);
     data_accessor*  accessor_1 = new data_accessor(batch_size);
     data_accessor* accessor_2 = new data_accessor(batch_size);
     std::string threadname_1 = "first";
@@ -112,25 +130,41 @@ int main(int argc, char * argv[]){
     std::thread worker_2(sorter, threadname_2, accessor_2, 1);
     std::ifstream f(filename, std::ios::binary | std::ios::in);
     int batch_num = 0, count_1 = 1, count_2 = 1;
-    while(count_1 || count_2){
+    while(count_1 || count_2) {
         {
+            {
+                std::unique_lock<std::mutex> lock(accessor_1->mutex);
+                vars[0].wait(lock, [accessor_1]()
+                {
+                    return accessor_1->read;
+                });
+                accessor_1->read = false;
+            }
             std::lock_guard<std::mutex> lock(accessor_1->mutex);
             f.seekg(batch_num++ * sizeof(uint64_t) * batch_size);
             f.read((char*) accessor_1->buf, sizeof(uint64_t) * batch_size);
             count_1 = f.gcount() / sizeof(uint64_t);
             accessor_1->size = count_1;
-            accessor_1->ready = true;
+            accessor_1->prepared = true;
             if(count_1 == 0)
                 accessor_1->end = true;
         }
         vars[0].notify_one();
         {
+            {
+                std::unique_lock<std::mutex> lock(accessor_2->mutex);
+                vars[0].wait(lock, [accessor_2]()
+                {
+                    return accessor_2->read;
+                });
+                accessor_2->read = false;
+            }
             std::lock_guard<std::mutex> lock(accessor_2->mutex);
             f.seekg(batch_num++ * sizeof(uint64_t) * batch_size);
             f.read((char*) accessor_2->buf, sizeof(uint64_t) * batch_size);
             count_2 = f.gcount() / sizeof(uint64_t);
             accessor_2->size = count_2;
-            accessor_2->ready = true;
+            accessor_2->prepared = true;
             if(count_2 == 0)
                 accessor_2->end = true;
         }
@@ -141,5 +175,7 @@ int main(int argc, char * argv[]){
     worker_2.join();
     merge(threadname_1 + std::string(".bin"), threadname_2 + std::string(".bin"), std::string("main"));
     std::rename((threadname_2 + std::string(".bin")).c_str(), "result.bin");
+    delete accessor_1;
+    delete accessor_2;
     return 0;
 }

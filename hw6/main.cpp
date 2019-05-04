ddf0e96e165973513397e50d147b40cc5df23a06
fibersel <0583463@gmail.com>
2019-05-04 21:34:02 +0300

    using stack instead of heap

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -80,26 +80,26 @@ void merge(const std::string& filename, const std::string& resulting, const std:
 
 
 
-void sorter(const std::string& name, data_accessor* access, int id) {
+void sorter(const std::string& name, data_accessor& access, int id) {
     uint64_t* buf = new uint64_t[batch_size];
     int size;
     int files_ctr = 0;
     vars[id].notify_one();
     while(true) {
         {
-            std::unique_lock<std::mutex> lock(access->mutex);
+            std::unique_lock<std::mutex> lock(access.mutex);
             vars[id].wait(lock,
-                          [access]()
+                          [&access]()
             {
-                return access->prepared;
+                return access.prepared;
             });
-            access->prepared = false;
-            access->read = true;
+            access.prepared = false;
+            access.read = true;
             vars[id].notify_one();
-            if(access->end)
+            if(access.end)
                 break;
-            size = access->size;
-            std::copy(access->buf, access->buf + size, buf);
+            size = access.size;
+            std::copy(access.buf, access.buf + size, buf);
         }
 
         std::sort(buf, buf + size);
@@ -122,51 +122,51 @@ int main(int argc, char * argv[]) {
     if (argc != 2)
         return -1;
     std::string filename(argv[1]);
-    data_accessor*  accessor_1 = new data_accessor(batch_size);
-    data_accessor* accessor_2 = new data_accessor(batch_size);
+    data_accessor  accessor_1(batch_size);
+    data_accessor  accessor_2(batch_size);
     std::string threadname_1 = "first";
     std::string threadname_2 = "second";
-    std::thread worker_1(sorter, threadname_1, accessor_1, 0);
-    std::thread worker_2(sorter, threadname_2, accessor_2, 1);
+    std::thread worker_1(sorter, threadname_1, std::ref(accessor_1), 0);
+    std::thread worker_2(sorter, threadname_2, std::ref(accessor_2), 1);
     std::ifstream f(filename, std::ios::binary | std::ios::in);
     int batch_num = 0, count_1 = 1, count_2 = 1;
     while(count_1 || count_2) {
         {
             {
-                std::unique_lock<std::mutex> lock(accessor_1->mutex);
-                vars[0].wait(lock, [accessor_1]()
+                std::unique_lock<std::mutex> lock(accessor_1.mutex);
+                vars[0].wait(lock, [&accessor_1]()
                 {
-                    return accessor_1->read;
+                    return accessor_1.read;
                 });
-                accessor_1->read = false;
+                accessor_1.read = false;
             }
-            std::lock_guard<std::mutex> lock(accessor_1->mutex);
+            std::lock_guard<std::mutex> lock(accessor_1.mutex);
             f.seekg(batch_num++ * sizeof(uint64_t) * batch_size);
-            f.read((char*) accessor_1->buf, sizeof(uint64_t) * batch_size);
+            f.read((char*) accessor_1.buf, sizeof(uint64_t) * batch_size);
             count_1 = f.gcount() / sizeof(uint64_t);
-            accessor_1->size = count_1;
-            accessor_1->prepared = true;
+            accessor_1.size = count_1;
+            accessor_1.prepared = true;
             if(count_1 == 0)
-                accessor_1->end = true;
+                accessor_1.end = true;
         }
         vars[0].notify_one();
         {
             {
-                std::unique_lock<std::mutex> lock(accessor_2->mutex);
-                vars[0].wait(lock, [accessor_2]()
+                std::unique_lock<std::mutex> lock(accessor_2.mutex);
+                vars[0].wait(lock, [&accessor_2]()
                 {
-                    return accessor_2->read;
+                    return accessor_2.read;
                 });
-                accessor_2->read = false;
+                accessor_2.read = false;
             }
-            std::lock_guard<std::mutex> lock(accessor_2->mutex);
+            std::lock_guard<std::mutex> lock(accessor_2.mutex);
             f.seekg(batch_num++ * sizeof(uint64_t) * batch_size);
-            f.read((char*) accessor_2->buf, sizeof(uint64_t) * batch_size);
+            f.read((char*) accessor_2.buf, sizeof(uint64_t) * batch_size);
             count_2 = f.gcount() / sizeof(uint64_t);
-            accessor_2->size = count_2;
-            accessor_2->prepared = true;
+            accessor_2.size = count_2;
+            accessor_2.prepared = true;
             if(count_2 == 0)
-                accessor_2->end = true;
+                accessor_2.end = true;
         }
         vars[1].notify_one();
     }
@@ -175,7 +175,5 @@ int main(int argc, char * argv[]) {
     worker_2.join();
     merge(threadname_1 + std::string(".bin"), threadname_2 + std::string(".bin"), std::string("main"));
     std::rename((threadname_2 + std::string(".bin")).c_str(), "result.bin");
-    delete accessor_1;
-    delete accessor_2;
     return 0;
 }
